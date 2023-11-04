020b12428ae1fc86fc5b4aae4ac208830e24c25b
FJavier117 <fjmendez07@gmail.com>
2023-11-04 12:10:08 -0600

    funciones js

---
+++ funciones-1.js
@@ -0,0 +1,20 @@
+//Funciones en JS
+/*
+    Sintaxis base function + nombre ( parametros){}
+    - Los procedimiento los procedimiento no retornan valores 
+    - Las funciones tiene un valor de retorno
+    TIPOS DE FUNCIONES
+    - Funciones definidas por el usuario
+    - Funciones dentro del lenguaje (Funciones incorporadas / Built-in)
+ */
+
+//Procedimiento
+function imprimirMensaje(mensaje) {
+    console.log(`Mensaje:  ${mensaje}`);
+}
+
+//Llamada a procedimiento
+imprimirMensaje('Hola esto es un ejemplo');
+
+
+
---
+++ funciones-2.js
@@ -0,0 +1,13 @@
+//Funciones en JS
+
+//Declaramos una función
+function sumar(a,b){
+    let resultado = a + b;
+    return resultado;
+}
+
+//Llamada a función
+let argA = 3, argB = 5;
+let resultado = sumar(argA,argB);
+console.log("Resultado de la suma: ", resultado);
+
